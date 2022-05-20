2eb6f256e51b51da6defbe982c49063b3d55d577
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-05-20 17:36:15 -0400

    About Section

--- js/app.js
+++ js/app.js
@@ -17,4 +17,5 @@ let sr = ScrollReveal({
     duration: 2500, 
     distance: "60px",  
 });  
-    sr.reveal(".showcase-info", {delay: 600}); 
+    sr.reveal(".showcase-info", {delay: 600});
+    sr.reveal(".showcase-image", { origin: "top", delay: 700}); 
