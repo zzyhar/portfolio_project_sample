e0f10825a36fd2246a140bcbe54afc41738b2fea
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-05-27 00:49:43 -0400

    Modal Pop Uo Animation

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ const progress_bars =  document.querySelectorAll(".skills svg circle");
 const ml_section = document.querySelector(".milestones"); 
 const ml_counter = document.querySelectorAll(".number span"); 
 
+const prt_section = document.querySelector(".portfolio"); 
+const zoom_icons = document.querySelectorAll(".zoom-icon");
+const modal_overlay = document.querySelector(".modal-overlay"); 
+const images = document.querySelectorAll(".images img");
+const prev_btn = document.querySelector(".prev-btn"); 
+const next_btn = document.querySelector(".next-btn"); 
+
 
 window.addEventListener("scroll", () => {
     if(!skillsPlayed) skillsCounter();
@@ -103,3 +110,44 @@ let mixer = mixitup(".portfolio-gallery", {
         duration: 500, 
     },
 }); 
+
+/* -------------- Modal Pop Uo Animation -------------- */
+
+let currentIndex = 0;
+
+zoom_icons.forEach((icn, i) => 
+    icn.addEventListener("click", () => {
+    prt_section.classList.add("open")
+    document.body.classList.add("stopScrolling");
+    currentIndex = i;
+    changeImage(currentIndex);
+  })
+);
+
+modal_overlay.addEventListener("click", () =>{
+  prt_section.classList.remove("open")
+  document.body.classList.remove("stopScrolling");
+});
+
+prev_btn.addEventListener("click", () =>{
+    if (currentIndex === 0){
+        currentIndex = 5
+    }
+    else{
+    currentIndex--;}
+    changeImage(currentIndex);
+});
+
+next_btn.addEventListener("click", () =>{
+    if (currentIndex === 5){
+        currentIndex = 0
+    }
+    else{
+    currentIndex++;}
+    changeImage(currentIndex);
+});
+
+function changeImage(index){
+    images.forEach(img => img.classList.remove("showImage"));
+    images[index].classList.add("showImage");    
+}
